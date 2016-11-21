be0bd8e0b2734fba67cf44f1bc88c28497107f16
Michal Moskal <michal@moskal.me>
2016-11-21 18:27:48 +0000

    Reset USB id to what it was

--- boards/zero/board_config.h
+++ boards/zero/board_config.h
@@ -8,8 +8,9 @@
 #define VOLUME_LABEL "ARDUINO-M0"
 
 //#define USB_VID 0x2341
+//#define USB_PID 0x024D
 #define USB_VID 0x03EB
-#define USB_PID 0x024D
+#define USB_PID 0x612C
 
 #define BULB_PORT 0
 #define BULB_PIN 17
