245b8c1c7ed2ccac8a368a93e3a2e45d757603a5
Peli de Halleux <jhalleux@microsoft.com>
2016-12-02 11:25:15 -0800

    support for burning boards

--- scripts/burn.sh
+++ scripts/burn.sh
@@ -1,2 +1,2 @@
 sh scripts/openocd.sh \
-	-c "telnet_port disabled; init; halt; at91samd bootloader 0; program {{build/zero/uf2-bootloader.bin}} verify reset; shutdown "
+	-c "telnet_port disabled; init; halt; at91samd bootloader 0; program {{build/$1/uf2-bootloader.bin}} verify reset; shutdown "
