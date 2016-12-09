e763a5fb59acd4dc89c9a961a5b00dd1e286e7bc
Michal Moskal <michal@moskal.me>
2016-12-09 19:22:06 +0100

    Using new HF2 spec

--- src/hid.c
+++ src/hid.c
@@ -11,6 +11,8 @@ typedef struct {
         uint8_t buf[FLASH_ROW_SIZE + 64];
         uint32_t buf32[(FLASH_ROW_SIZE + 64) / 4];
         uint16_t buf16[(FLASH_ROW_SIZE + 64) / 2];
+        HF2_Command cmd;
+        HF2_Response resp;
     };
 } HID_InBuffer;
 
@@ -21,13 +23,15 @@ int recv_hid(HID_InBuffer *pkt) {
     assert(pkt->pbuf.ptr == 0 && pkt->pbuf.size > 0);
     pkt->pbuf.size = 0;
     uint8_t tag = pkt->pbuf.buf[0];
-    assert(pkt->size == 0 || (tag & 0x80));
+    // serial packets not allowed when in middle of command packet
+    assert(pkt->size == 0 || !(tag & HF2_FLAG_SERIAL_OUT));
     memcpy(pkt->buf + pkt->size, pkt->pbuf.buf + 1, tag & HF2_SIZE_MASK);
     pkt->size += tag & HF2_SIZE_MASK;
     assert(pkt->size <= sizeof(pkt->buf));
-    if (tag & 0x40) {
+    tag &= HF2_FLAG_MASK;
+    if (tag != HF2_FLAG_CMDPKT_BODY) {
+        pkt->serial = tag - 0x40;
         int sz = pkt->size;
-        pkt->serial = (tag & HF2_FLAG_MASK) == HF2_FLAG_SERIAL;
         pkt->size = 0;
         return sz;
     }
@@ -42,10 +46,10 @@ void send_hid(const void *data, int size, int ep) {
         int s;
         if (size <= 63) {
             s = size;
-            buf[0] = HF2_FLAG_PKT_LAST | size;
+            buf[0] = HF2_FLAG_CMDPKT_LAST | size;
         } else {
             s = 63;
-            buf[0] = HF2_FLAG_PKT_BODY | 63;
+            buf[0] = HF2_FLAG_CMDPKT_BODY | 63;
         }
         memcpy(buf + 1, ptr, s);
         USB_WriteCore(buf, sizeof(buf), ep, true);
@@ -59,7 +63,7 @@ void send_hid(const void *data, int size, int ep) {
 void send_hid_response(HID_InBuffer *pkt, const void *data, int size) {
     logval("sendresp", size);
     if (data)
-        memcpy(pkt->buf32 + 1, data, size);
+        memcpy(pkt->resp.data8, data, size);
     send_hid(pkt->buf, 4 + size, pkt->ep);
 }
 
@@ -70,7 +74,7 @@ static void checksum_pages(HID_InBuffer *pkt, int start, int num) {
         for (int j = 0; j < FLASH_ROW_SIZE; ++j) {
             crc = add_crc(*data++, crc);
         }
-        pkt->buf16[i + 2] = crc;
+        pkt->resp.data16[i] = crc;
     }
     send_hid_response(pkt, 0, num * 2);
 }
@@ -94,15 +98,28 @@ void process_core(HID_InBuffer *pkt) {
     logwritenum(sz);
     logval(" CMD", pkt->buf32[0]);
 
-    switch (pkt->buf16[0]) {
+    // one has to be careful dealing with these, as they share memory
+    HF2_Command *cmd = &pkt->cmd;
+    HF2_Response *resp = &pkt->resp;
+
+    uint32_t cmdId = cmd->command_id;
+    uint32_t tmp;
+    resp->tag = cmd->tag;
+    resp->status16 = HF2_STATUS_OK;
+
+#define checkDataSize(str, add) assert(sz == 8 + sizeof(cmd->str) + (add))
+
+    switch (cmdId) {
     case HF2_CMD_INFO:
         send_hid_response(pkt, infoUf2File, strlen(infoUf2File));
         return;
 
     case HF2_CMD_BININFO:
-        pkt->buf32[1] = HF2_MODE_BOOTLOADER; // bootloader
-        pkt->buf32[2] = FLASH_ROW_SIZE;
-        send_hid_response(pkt, 0, 8);
+        resp->bininfo.mode = HF2_MODE_BOOTLOADER;
+        resp->bininfo.flash_page_size = FLASH_ROW_SIZE;
+        resp->bininfo.flash_num_pages = FLASH_SIZE / FLASH_ROW_SIZE;
+        resp->bininfo.max_message_size = sizeof(pkt->buf);
+        send_hid_response(pkt, 0, sizeof(resp->bininfo));
         return;
 
     case HF2_CMD_RESET_INTO_APP:
@@ -115,31 +132,32 @@ void process_core(HID_InBuffer *pkt) {
         // userspace app should reboot into bootloader on this command; we just ignore it
         break;
     case HF2_CMD_WRITE_FLASH_PAGE:
-        assert(sz == 8 + FLASH_ROW_SIZE);
+        checkDataSize(write_flash_page, FLASH_ROW_SIZE);
         // first send ACK and then start writing, while getting the next packet
         send_hid_response(pkt, 0, 0);
-        if (pkt->buf32[1] >= APP_START_ADDRESS) {
-            flash_write_row((void *)pkt->buf32[1], pkt->buf32 + 2);
+        if (cmd->write_flash_page.target_addr >= APP_START_ADDRESS) {
+            flash_write_row((void *)cmd->write_flash_page.target_addr, cmd->write_flash_page.data);
         }
         return;
-    case HF2_CMD_MEM_WRITE_WORDS:
-        assert(sz == 8 + FLASH_ROW_SIZE);
-        copy_words((void *)pkt->buf32[1], pkt->buf32 + 3, pkt->buf32[2]);
+    case HF2_CMD_WRITE_WORDS:
+        checkDataSize(write_words, cmd->write_words.num_words << 2);
+        copy_words((void *)cmd->write_words.target_addr, cmd->write_words.words,
+                   cmd->write_words.num_words);
         break;
-    case HF2_CMD_MEM_READ_WORDS:
-        assert(sz == 12);
-        sz = pkt->buf32[2] << 2;
-        copy_words(pkt->buf32 + 1, (void *)pkt->buf32[1], pkt->buf32[2]);
-        send_hid_response(pkt, 0, sz);
+    case HF2_CMD_READ_WORDS:
+        checkDataSize(read_words, 0);
+        tmp = cmd->read_words.num_words;
+        copy_words(resp->data32, (void *)cmd->read_words.target_addr, tmp);
+        send_hid_response(pkt, 0, tmp << 2);
         return;
     case HF2_CMD_CHKSUM_PAGES:
-        assert(sz == 12);
-        checksum_pages(pkt, pkt->buf32[1], pkt->buf32[2]);
+        checkDataSize(chksum_pages, 0);
+        checksum_pages(pkt, cmd->chksum_pages.target_addr, cmd->chksum_pages.num_pages);
         return;
 
     default:
         // command not understood
-        pkt->buf16[0] |= 0x8000;
+        resp->status16 = HF2_STATUS_INVALID_CMD;
         break;
     }
 
@@ -159,6 +177,5 @@ void process_hid() {
     process_core(&webbufData);
 #endif
 }
-
 
 #endif
