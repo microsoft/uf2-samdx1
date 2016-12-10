e5415636d1e0a869599572d61f2fa1c5d3b3d771
Michal Moskal <michal@moskal.me>
2016-12-10 13:40:12 +0100

    Fix logs output

--- scripts/logsaddr.js
+++ scripts/logsaddr.js
@@ -1,5 +1,5 @@
 "use strict";
 let fs = require("fs")
 let s = fs.readFileSync(process.argv[2], "utf8")
-let m = /logStoreUF2\n\r?.*?(0x[a-f0-9]+)/.exec(s)
+let m = /(0x[a-f0-9]+)\s*logStoreUF2\s*$/m.exec(s)
 console.log(m[1])
