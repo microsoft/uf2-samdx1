ca7a3550272de686c6afb49b42310de7ab4a37a9
Michal Moskal <michal@moskal.me>
2017-02-07 17:10:52 +0000

    Fix applet extraction for 32 bit instructions

--- scripts/genapplet.js
+++ scripts/genapplet.js
@@ -1,6 +1,5 @@
 let fs = require("fs")
 let s = fs.readFileSync(process.argv[2], "utf8")
-let pref = ""
 let infun = false
 let words = []
 for (let l of s.split(/\n/)) {
@@ -8,28 +7,41 @@ for (let l of s.split(/\n/)) {
     if (m && m[1] == process.argv[3]) infun = true
     if (/^Disassembly/.test(l)) infun = false
     if (!infun) continue
-    m = /^\s*[0-9a-f]+:\s+([0-9a-f]+)\s+/.exec(l)
+    m = /^\s*[0-9a-f]+:\s+([0-9a-f]+)( ([0-9a-f]{4}))?\s+/.exec(l)
     if (m) {
         let n = m[1]
-        if (n.length == 4) {
-            if (pref) {
-                words.push("0x" + n + pref)
-                pref = ""
-            } else {
-                pref = n
-            }
-        } else if (n.length == 8) {
-            if (pref) throw new Error()
-            words.push("0x" + n)
+        words.push(n)
+        if (m[3])
+            words.push(m[3])
+        if (n.length == 4 || n.length == 8) {
+            // ok
         } else {
             throw new Error()
         }
     }
 }
 
+let ww = []
+let pref = ""
+for (let w of words) {
+    if (w.length == 8) {
+        if (pref) throw new Error()
+        ww.push("0x" + w)
+    } else {
+        if (pref) {
+            ww.push("0x" + w + pref)
+            pref = ""
+        } else {
+            pref = w
+        }
+    }
+}
+
+words = ww
+
 let r = ""
 for (let i = 0; i < words.length; i++) {
-    if (i%6 == 0) r += "\n"
+    if (i % 6 == 0) r += "\n"
     r += words[i] + ", "
 }
 
