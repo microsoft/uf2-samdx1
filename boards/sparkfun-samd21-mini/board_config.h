e8c4cfc25f97ff4c7243640e754849b6a915c9c0
Tom Keddie <git@bronwenandtom.com>
2017-04-28 15:47:12 -0700

    First pass at adding sparkfun support

---
+++ boards/sparkfun-samd21-mini/board_config.h
@@ -0,0 +1,18 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define __SAMD21G18A__ 1
+
+#define VENDOR_NAME "Sparkfun"
+#define PRODUCT_NAME "SAMD21 Mini Breakout"
+#define VOLUME_LABEL "SAMD21MINI"
+#define INDEX_URL "https://www.sparkfun.com/products/13664"
+#define BOARD_ID "SAMD21MINI-v0"
+
+#define USB_VID 0x1B4F
+#define USB_PID 0x8D21
+
+#define LED_PIN PIN_PA17
+
+
+#endif
