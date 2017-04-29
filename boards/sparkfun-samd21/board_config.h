a8150c2eeed408181604997e544eeb6879053654
Tom Keddie <git@bronwenandtom.com>
2017-04-28 17:12:17 -0700

    Renamed to reflect boath dev breakout and mini breakout have same vid/pid

--- boards/sparkfun-samd21/board_config.h
+++ boards/sparkfun-samd21/board_config.h
@@ -4,15 +4,15 @@
 #define __SAMD21G18A__ 1
 
 #define VENDOR_NAME "Sparkfun"
-#define PRODUCT_NAME "SAMD21 Mini Breakout"
-#define VOLUME_LABEL "SAMD21MINI"
+#define PRODUCT_NAME "SAMD21 Dev and Mini Breakout"
+#define VOLUME_LABEL "SPARKFUN"
+// also https://www.sparkfun.com/products/13672
 #define INDEX_URL "https://www.sparkfun.com/products/13664"
-#define BOARD_ID "SAMD21MINI-v0"
+#define BOARD_ID "SAMD21-v0"
 
 #define USB_VID 0x1B4F
 #define USB_PID 0x8D21
 
 #define LED_PIN PIN_PA17
-
 
 #endif
