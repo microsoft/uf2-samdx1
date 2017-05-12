c3fe2a8769ddb5862be25f8a3b8c00807055ff68
ladyada <limor@ladyada.net>
2017-05-12 12:19:22 -0400

    fix PID, url, and name (to save space :)

--- boards/circuitplay/board_config.h
+++ boards/circuitplay/board_config.h
@@ -5,13 +5,14 @@
 #define CRYSTALLESS    1
 
 #define VENDOR_NAME "Adafruit Industries"
-#define PRODUCT_NAME "Circuit Playground Express"
+#define PRODUCT_NAME "CPlay Express"
 #define VOLUME_LABEL "CPLAYBOOT"
+#define INDEX_URL "http://adafru.it/3333"
 
 #define BOARD_ID "SAMD21G18A-CPlay-v0"
 
 #define USB_VID 0x239A
-#define USB_PID 0x0019
+#define USB_PID 0x0018
 
 #define LED_PIN PIN_PA17
 //#define LED_TX_PIN PIN_PA27
