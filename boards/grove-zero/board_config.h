f1a9ac1484fed20bb9a3dcd480330511b7870f79
jinyuan.weng <jinyuan.weng@seeed.cc>
2017-09-29 09:54:31 +0800

    Add grove zero board config file.

---
+++ boards/grove-zero/board_config.h
@@ -0,0 +1,20 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define __SAMD21G18A__ 1
+
+#define VENDOR_NAME "Seeed Studio"
+#define PRODUCT_NAME "Grove Zero"
+#define VOLUME_LABEL "Grove Zero"
+#define INDEX_URL "https://www.seeedstudio.com/"
+
+#define BOARD_ID "SAMD21G18A-Grove-Zero-v1.0"
+
+#define USB_VID 0x2886
+#define USB_PID 0x000D
+
+#define LED_PIN PIN_PA17
+#define LED_TX_PIN PIN_PA27
+#define LED_RX_PIN PIN_PB03
+
+#endif
