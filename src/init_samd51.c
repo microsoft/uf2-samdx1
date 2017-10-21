6cac35c955418d33f51bdf2276bf6dabbcd52607
Scott Shawcroft <scott.shawcroft@gmail.com>
2017-10-20 22:44:01 -0700

    Enumerates ok but flash doesn't work.

--- src/init_samd51.c
+++ src/init_samd51.c
@@ -1,24 +1,12 @@
 #include "uf2.h"
 
-#define GENERIC_CLOCK_GENERATOR_MAIN      (0u)
-#define GENERIC_CLOCK_GENERATOR_XOSC32K   (3u)
-
 void system_init(void) {
     /* Set 1 Flash Wait State for 48MHz */
     NVMCTRL->CTRLA.reg |= NVMCTRL_CTRLA_RWS(0);
 
-    // 1) Enable XOSC32K clock (External on-board 32.768Hz oscillator)
-    OSC32KCTRL->XOSC32K.reg = OSC32KCTRL_XOSC32K_ENABLE |
-                              OSC32KCTRL_XOSC32K_EN32K |
-                              OSC32KCTRL_XOSC32K_CGM_XT |
-                              OSC32KCTRL_XOSC32K_XTALEN;
-
-    while( OSC32KCTRL->STATUS.bit.XOSC32KRDY == 0 ) {
-        /* Wait for oscillator to be ready */
-    }
-
-    OSC32KCTRL->RTCCTRL.bit.RTCSEL = OSC32KCTRL_RTCCTRL_RTCSEL_ULP1K;
-
+    // Output GCLK0 to Metro M4 D5. This way we can see if/when we mess it up.
+    PORT->Group[1].PINCFG[14].bit.PMUXEN = true;
+    PORT->Group[1].PMUX[7].bit.PMUXE = 12;
 
     /* Software reset the module to ensure it is re-initialized correctly */
     /* Note: Due to synchronization, there is a delay from writing CTRL.SWRST until the reset is complete.
@@ -29,29 +17,22 @@ void system_init(void) {
         /* wait for reset to complete */
     }
 
-    // 2) Put XOSC32K as source of Generic Clock Generator 3
-    GCLK->GENCTRL[GENERIC_CLOCK_GENERATOR_XOSC32K].reg =
-        GCLK_GENCTRL_SRC(GCLK_GENCTRL_SRC_XOSC32K) | // generic clock gen 3
-        GCLK_GENCTRL_GENEN;
+    // Temporarily switch the CPU to the internal 32k oscillator while we
+    // reconfigure the DFLL.
+    GCLK->GENCTRL[0].reg = GCLK_GENCTRL_SRC(GCLK_GENCTRL_SRC_OSCULP32K) |
+                           GCLK_GENCTRL_OE |
+                           GCLK_GENCTRL_GENEN;
 
-    while (GCLK->SYNCBUSY.bit.GENCTRL3) {
+    while (GCLK->SYNCBUSY.bit.GENCTRL0) {
         /* Wait for synchronization */
     }
 
-    // 3) Put Generic Clock Generator 3 as source for Generic Clock Gen 0 (DFLL48M reference)
-    GCLK->GENCTRL[0].reg =
-        GCLK_GENCTRL_SRC(GCLK_GENCTRL_SRC_OSCULP32K) | GCLK_GENCTRL_GENEN;
-
-    // 4) Enable DFLL48M clock
-
-    /* DFLL Configuration in Open Loop mode */
-
+    // Configure the DFLL for USB clock recovery.
     OSCCTRL->DFLLCTRLA.reg = 0;
-    //GCLK->PCHCTRL[OSCCTRL_GCLK_ID_DFLL48].reg = (1 << GCLK_PCHCTRL_CHEN_Pos) | GCLK_PCHCTRL_GEN(GCLK_PCHCTRL_GEN_GCLK3_Val);
 
     OSCCTRL->DFLLMUL.reg = OSCCTRL_DFLLMUL_CSTEP( 0x1 ) |
                            OSCCTRL_DFLLMUL_FSTEP( 0x1 ) |
-                           OSCCTRL_DFLLMUL_MUL( 0 );
+                           OSCCTRL_DFLLMUL_MUL( 0xBB80 );
 
     while (OSCCTRL->DFLLSYNC.bit.DFLLMUL) {
         /* Wait for synchronization */
@@ -78,7 +59,7 @@ void system_init(void) {
     }
 
     // 5) Switch Generic Clock Generator 0 to DFLL48M. CPU will run at 48MHz.
-    GCLK->GENCTRL[GENERIC_CLOCK_GENERATOR_MAIN].reg =
+    GCLK->GENCTRL[0].reg =
         GCLK_GENCTRL_SRC(GCLK_GENCTRL_SRC_DFLL) |
                          GCLK_GENCTRL_IDC |
                          GCLK_GENCTRL_OE |
