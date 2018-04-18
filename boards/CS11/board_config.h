df9936d8e069119ef222624b12165b62fc60d640
brendanvanbreda <brendanvanbreda@users.noreply.github.com>
2018-04-18 09:31:49 +0200

    Create board_config.h

---
+++ boards/CS11/board_config.h
@@ -0,0 +1,16 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "XinaBox Limited"
+#define PRODUCT_NAME "CS11"
+#define VOLUME_LABEL "CS11"
+#define INDEX_URL "https://xinabox.cc/products/cc11"
+
+#define BOARD_ID "SAMD21G18A-CS11-v0"
+
+//#define USB_VID 0x2341
+//#define USB_PID 0x024D
+
+#define LED_PIN PIN_PA08
+
+#endif
