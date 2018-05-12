32e8aae2bea1e19cfaceec465ad14414ce3fa400
ladyada <limor@ladyada.net>
2018-05-12 17:30:33 -0400

    generic feather m0

---
+++ boards/feather_m0/board_config.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Adafruit Industries"
+#define PRODUCT_NAME "Feather M0"
+#define VOLUME_LABEL "FEATHERBOOT"
+#define INDEX_URL "http://adafru.it/"
+#define BOARD_ID "SAMD21G18A-Feather-v0"
+
+#define USB_VID 0x239A
+#define USB_PID 0x0015
+
+#define LED_PIN PIN_PA17
+
+#endif
