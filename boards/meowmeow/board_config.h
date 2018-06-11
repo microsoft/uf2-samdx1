05ecdd6dbecdbc4262dcfac15a556385aeed8da9
sabas1080 <s@theinventorhouse.org>
2018-06-11 13:07:03 -0500

    Add Board MeowMeow

---
+++ boards/meowmeow/board_config.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Electronic Cats"
+#define PRODUCT_NAME "Meow Meow"
+#define VOLUME_LABEL "MEOWMEOW"
+#define INDEX_URL "https://electroniccats.com/howtomeow"
+#define BOARD_ID "SAMD21G18A-Meow-Meow"
+
+#define USB_VID 0x1209   // http://pid.codes/
+#define USB_PID 0xBAB1   // Meow Meow PID
+
+#define LED_PIN PIN_PB23
+
+#endif
