9c88421a1a61f54c0cf76215eadf3c9e13b4f0f7
ShawnHymel <hymelsr@vt.edu>
2018-11-13 14:06:06 -0600

    Added MKRZero board

---
+++ boards/mkrzero/board_config.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Arduino LLC"
+#define PRODUCT_NAME "Arduino MKRZero"
+#define VOLUME_LABEL "MKRZEROBOOT"
+
+#define BOARD_ID "SAMD21G18A-MKRZero-v0"
+
+//#define USB_VID 0x2341
+//#define USB_PID 0x804F
+
+#define LED_PIN PIN_PB08
+
+#endif
