ee42b76f3ea674760f6e2a34ade12df893d81c8b
Sandeepan Sengupta <sengupta.sandeepan@gmail.com>
2019-01-04 18:08:31 +0530

    Fixed board

---
+++ boards/neutrino/board_config.h
@@ -0,0 +1,16 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Sandeepan Sengupta"
+#define PRODUCT_NAME "Neutrino"
+#define VOLUME_LABEL "neutrino"
+#define INDEX_URL "http://sandeepan.info"
+#define BOARD_ID "Neutrino by Sandeepan"
+
+#define USB_VID 0x1209   // pid.codes
+#define USB_PID 0xBCDE
+
+#define LED_PIN PIN_PA17
+
+
+#endif
