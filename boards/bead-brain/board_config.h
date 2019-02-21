aaaa4c29005ce5dfcaf7c876d0c9bdd3331d547a
Teddy Seyed <t-tesey@microsoft.com>
2019-02-21 10:07:02 -0800

    Adding initial configs for some of the beads.

---
+++ boards/bead-brain/board_config.h
@@ -0,0 +1,18 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Microsoft Research"
+#define PRODUCT_NAME "Project Brookdale"
+#define VOLUME_LABEL "BRAIN_HUB"
+#define INDEX_URL "https://www.microsoft.com/en-us/research/"
+
+#define BOARD_ID "SAMD21G18A-BrainHub-v0"
+
+#define USB_VID 0x239A
+#define USB_PID 0x0018
+
+#define LED_PIN PIN_PA17
+
+#endif
---
+++ boards/bead-motion-v2/board_config.h
@@ -0,0 +1,18 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Microsoft Research"
+#define PRODUCT_NAME "Project Brookdale"
+#define VOLUME_LABEL "MOTION_BEAD_V2"
+#define INDEX_URL "https://www.microsoft.com/en-us/research/"
+
+#define BOARD_ID "SAMD21G18A-MotionBead-v2"
+
+#define USB_VID 0x239A
+#define USB_PID 0x0018
+
+#define LED_PIN PIN_PA17
+
+#endif
