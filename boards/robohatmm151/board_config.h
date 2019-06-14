4fdf0fece2a47934b7d5fb1aeace06ffc269278e
wallarug <wallarug@coliemore.com.au>
2019-06-14 23:30:56 +1000

    robohatmm1 samd51: tested build settings.  UART enabled on UART header

--- boards/robohatmm151/board_config.h
+++ boards/robohatmm151/board_config.h
@@ -14,4 +14,15 @@
 
 #define LED_PIN PIN_PA21
 
+#define BOOT_USART_MODULE                 SERCOM0
+#define BOOT_USART_MASK                   APBAMASK
+#define BOOT_USART_BUS_CLOCK_INDEX        MCLK_APBAMASK_SERCOM0
+#define BOOT_USART_PAD_SETTINGS           UART_RX_PAD1_TX_PAD0
+#define BOOT_USART_PAD3                   PINMUX_UNUSED
+#define BOOT_USART_PAD2                   PINMUX_UNUSED
+#define BOOT_USART_PAD1                   PINMUX_PA05D_SERCOM0_PAD1
+#define BOOT_USART_PAD0                   PINMUX_PA04D_SERCOM0_PAD0
+#define BOOT_GCLK_ID_CORE                 SERCOM0_GCLK_ID_CORE
+#define BOOT_GCLK_ID_SLOW                 SERCOM0_GCLK_ID_SLOW
+
 #endif
