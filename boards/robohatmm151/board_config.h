1f86cd3f60edd6d8c3b6979db112adc89e2ea352
wallarug <wallarug@coliemore.com.au>
2019-06-18 20:48:17 +1000

    no serial

--- boards/robohatmm151/board_config.h
+++ boards/robohatmm151/board_config.h
@@ -14,15 +14,4 @@
 
 #define LED_PIN PIN_PA21
 
-#define BOOT_USART_MODULE                 SERCOM5
-#define BOOT_USART_MASK                   APBDMASK
-#define BOOT_USART_BUS_CLOCK_INDEX        MCLK_APBDMASK_SERCOM5
-#define BOOT_USART_PAD_SETTINGS           UART_RX_PAD1_TX_PAD0
-#define BOOT_USART_PAD3                   PINMUX_UNUSED
-#define BOOT_USART_PAD2                   PINMUX_UNUSED
-#define BOOT_USART_PAD1                   PINMUX_PB03D_SERCOM5_PAD1
-#define BOOT_USART_PAD0                   PINMUX_PB02D_SERCOM5_PAD0
-#define BOOT_GCLK_ID_CORE                 SERCOM5_GCLK_ID_CORE
-#define BOOT_GCLK_ID_SLOW                 SERCOM5_GCLK_ID_SLOW
-
 #endif
