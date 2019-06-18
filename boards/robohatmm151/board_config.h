a7a454132f68282103a31db1653d8488c2a846d4
wallarug <wallarug@coliemore.com.au>
2019-06-18 20:30:03 +1000

    updated uart port

--- boards/robohatmm151/board_config.h
+++ boards/robohatmm151/board_config.h
@@ -5,23 +5,23 @@
 
 #define VENDOR_NAME "Robotics Masters"
 #define PRODUCT_NAME "Robo HAT MM1"
-#define VOLUME_LABEL "ROBOBOOT"
+#define VOLUME_LABEL "ROBOM4BOOT"
 #define INDEX_URL "https://roboticsmasters.co"
-#define BOARD_ID "SAMD51G19A-robohat-v7"
+#define BOARD_ID "SAMD51G19A-robohat-v7a"
 
 #define USB_VID 0x1209
 #define USB_PID 0x4D44
 
 #define LED_PIN PIN_PA21
 
-#define BOOT_USART_MODULE                 SERCOM0
+#define BOOT_USART_MODULE                 SERCOM5
 #define BOOT_USART_MASK                   APBAMASK
-#define BOOT_USART_BUS_CLOCK_INDEX        MCLK_APBAMASK_SERCOM0
+#define BOOT_USART_BUS_CLOCK_INDEX        MCLK_APBAMASK_SERCOM5
 #define BOOT_USART_PAD_SETTINGS           UART_RX_PAD1_TX_PAD0
 #define BOOT_USART_PAD3                   PINMUX_UNUSED
 #define BOOT_USART_PAD2                   PINMUX_UNUSED
-#define BOOT_USART_PAD1                   PINMUX_PA05D_SERCOM0_PAD1
-#define BOOT_USART_PAD0                   PINMUX_PA04D_SERCOM0_PAD0
+#define BOOT_USART_PAD1                   PINMUX_PB03D_SERCOM5_PAD1
+#define BOOT_USART_PAD0                   PINMUX_PB02D_SERCOM5_PAD0
 #define BOOT_GCLK_ID_CORE                 SERCOM0_GCLK_ID_CORE
 #define BOOT_GCLK_ID_SLOW                 SERCOM0_GCLK_ID_SLOW
 
--- build/robohatmm151/uf2_version.h
+++
@@ -1,1 +0,0 @@
-#define UF2_VERSION_BASE "v3.6.0-2-g4fdf0fe"
