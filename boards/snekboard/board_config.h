1013bc682e8dd787cc2fffea510604340f0fb5bb
Keith Packard <keithp@keithp.com>
2019-06-27 23:50:04 -0700

    add snekboard
    Another samd21g18a board developed for snek and legos
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

---
+++ boards/snekboard/board_config.h
@@ -0,0 +1,19 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "keithp.com"
+#define PRODUCT_NAME "SnekBoard"
+#define VOLUME_LABEL "SNEKBOOT"
+#define INDEX_URL "https://keithp.com/snek"
+#define BOARD_ID "SAMD21G18A-SnekBoard-v0"
+
+#define USB_VID 0x239A
+#define USB_PID 0x001B
+
+#define LED_PIN PIN_PA02
+
+//#define BOARD_NEOPIXEL_PIN PIN_PB11
+//#define BOARD_NEOPIXEL_COUNT 2
+
+
+#endif
