4260c08e096156ec3cb516716293ebdce0ad81f3
wallarug <wallarug@coliemore.com.au>
2019-07-27 18:44:25 +1000

    final board changes

--- boards/robohatmm1/board_config.h
+++ boards/robohatmm1/board_config.h
@@ -7,7 +7,7 @@
 #define PRODUCT_NAME "Robo HAT MM1"
 #define VOLUME_LABEL "ROBOM0BOOT"
 #define INDEX_URL "https://roboticsmasters.co"
-#define BOARD_ID "SAMD21G18A-v4"
+#define BOARD_ID "SAMD21G18A-robohat-v8"
 
 #define USB_VID 0x1209
 #define USB_PID 0x4D44
