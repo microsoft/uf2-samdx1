accf14e78d25bef370fc6f52b7863ac4191a17d4
Keith Packard <keithp@keithp.com>
2019-07-28 14:06:05 -0700

    Update snekboard PID to official snekboard bootloader value
    Snekboard has been assigned the following PIDs:
    
    	PID 0x004D  # bootloader
    	PID 0x804D  # arduino
    	PID 0x804E  # circuitpython
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- boards/snekboard/board_config.h
+++ boards/snekboard/board_config.h
@@ -2,18 +2,17 @@
 #define BOARD_CONFIG_H
 
 #define VENDOR_NAME "keithp.com"
-#define PRODUCT_NAME "SnekBoard"
+#define PRODUCT_NAME "snekboard"
 #define VOLUME_LABEL "SNEKBOOT"
 #define INDEX_URL "https://keithp.com/snek"
-#define BOARD_ID "SAMD21G18A-SnekBoard-v0"
+#define BOARD_ID "SAMD21G18A-snekboard"
 
 #define USB_VID 0x239A
-#define USB_PID 0x001B
+#define USB_PID 0x004D
 
 #define LED_PIN PIN_PA02
 
 //#define BOARD_NEOPIXEL_PIN PIN_PB11
 //#define BOARD_NEOPIXEL_COUNT 2
-
 
 #endif
