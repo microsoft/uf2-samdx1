56ce473c04583c9b04105e5ef89c7e117c8a4a04
arturo182 <arturo182@tlen.pl>
2019-09-13 19:54:06 +0200

    Add Serpente board

---
+++ boards/serpente/board_config.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "arturo182"
+#define PRODUCT_NAME "Serpente"
+#define VOLUME_LABEL "SERPENTBOOT"
+#define INDEX_URL "https://serpente.solder.party/"
+#define BOARD_ID "SAMD21E18A-Serpente"
+
+#define USB_VID 0x239A
+#define USB_PID 0x0057
+
+#define LED_PIN PIN_PA23
+
+#endif
