b135f7d116f541d0299dd3bb6faaeb649e53e5bf
sabas1080 <s@theinventorhouse.org>
2019-11-06 15:39:59 -0600

    add bastWAN

---
+++ boards/bast-wan/board_config.h
@@ -0,0 +1,18 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define __SAML21J18B__ 1
+
+#define VENDOR_NAME "Generic Corp."
+#define PRODUCT_NAME "SAMD21 Board"
+#define VOLUME_LABEL "SAMD21"
+
+#define USB_VID 0x03EB   // Atmel
+#define USB_PID 0x2402   // Generic HID device
+
+#define BOARD_ID "SAML21J18B-Generic"
+
+#define LED_PIN PIN_PA17
+
+
+#endif
