64b1e5c7e13446144e89e5d479888830b7be802b
sabas1080 <s@theinventorhouse.org>
2019-11-21 12:51:43 -0600

    config bastwan

--- boards/bast-wan/board_config.h
+++ boards/bast-wan/board_config.h
@@ -3,16 +3,16 @@
 
 #define __SAML21J18B__ 1
 
-#define VENDOR_NAME "Generic Corp."
-#define PRODUCT_NAME "SAML21 Board"
-#define VOLUME_LABEL "SAML21"
+#define VENDOR_NAME "Electronic Cats"
+#define PRODUCT_NAME "BASTWAN Board"
+#define VOLUME_LABEL "BASTWAN"
 
 #define USB_VID 0x03EB   // Atmel
 #define USB_PID 0x2402   // Generic HID device
 
-#define BOARD_ID "SAML21J18B-Generic"
+#define BOARD_ID "BAST-WAN"
 
-#define LED_PIN PIN_PA17
+#define LED_PIN PIN_PA22
 
 
 #endif
