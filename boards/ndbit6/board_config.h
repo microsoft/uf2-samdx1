69afdc9270e0fd521cfff584341ebe50be018146
ndgarage <n0dgarage@gmail.com>
2019-12-02 10:01:42 -0700

    ndbit6-no-flash

--- boards/ndbit6/board_config.h
+++ boards/ndbit6/board_config.h
@@ -4,10 +4,10 @@
 #define CRYSTALLESS    1
 
 #define VENDOR_NAME "ndGarage"
-#define PRODUCT_NAME "ndbit6"
+#define PRODUCT_NAME "ndBit6"
 #define VOLUME_LABEL "ND6BOOT"
 #define INDEX_URL "https://ndgarage.github.io/"
-#define BOARD_ID "SAMD21E18A-NDBIT6"
+#define BOARD_ID "SAMD21E18A-ND6"
 
 #define USB_VID 0x239A
 #define USB_PID 0x0065
