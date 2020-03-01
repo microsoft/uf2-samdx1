71dfe62ff62563f577b027ff35d658a9f64a9bba
Chris Burton <Chris@8086.net>
2020-03-01 19:21:39 +0000

    Add support for Commander

---
+++ boards/8086_commander/board_config.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "8086 Consultancy"
+#define PRODUCT_NAME "COMMANDER"
+#define VOLUME_LABEL "CMDBOOT"
+#define INDEX_URL "https://8086.net/p/commander"
+#define BOARD_ID "SAMD21G18A-Commander-v0"
+
+#define USB_VID 0x3171
+#define USB_PID 0x0100
+
+#define LED_PIN PIN_PA06
+
+#endif
