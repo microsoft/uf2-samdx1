d8126f817513185bb1115d4a3f1ea35727eb8d30
sabas1080 <s@electroniccats.com>
2020-04-09 14:12:31 -0500

    add bast pro mini m0
    yeah yeah!

---
+++ boards/bast_pro_mini/board_config.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Electronic Cats SAPI de CV"
+#define PRODUCT_NAME "Bast Pro Mini M0"
+#define VOLUME_LABEL "Bast"
+#define INDEX_URL "http://electroniccats.com"
+#define BOARD_ID "bastpro"
+
+#define USB_VID 0x1209
+#define USB_PID 0xBAB3
+
+#define LED_PIN PIN_PA14
+
+#endif
