f3c447ef0ec14839c4f361bf5f8af6794f9a62bb
sabas1080 <s@electroniccats.com>
2020-04-09 14:09:30 -0500

    add escornabot makech
    return :v

---
+++ boards/escornabot_makech/board_config.h
@@ -0,0 +1,18 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Electronic Cats"
+#define PRODUCT_NAME "Escornabot Makech"
+#define VOLUME_LABEL "MAKECH"
+#define INDEX_URL "https://electroniccats.com"
+
+#define BOARD_ID "SAMD21G18A-escornabot-makech-v0"
+
+#define USB_VID 0x1209
+#define USB_PID 0xBAB6
+
+#define LED_PIN PIN_PA02
+
+#endif
