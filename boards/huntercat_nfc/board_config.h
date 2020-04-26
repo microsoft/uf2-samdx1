1ab66f6d2fac02d5c4cf38e2d5d76683df0e913a
Sabas <s@electroniccats.com>
2020-04-25 22:57:31 -0500

    Add HunterCat NFC

---
+++ boards/huntercat_nfc/board_config.h
@@ -0,0 +1,18 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Electronic Cats SAPI de CV"
+#define PRODUCT_NAME "HunterCat NFC"
+#define VOLUME_LABEL "HUNTERNFC"
+
+#define USB_VID 0x1209  
+#define USB_PID 0xBABA
+
+#define BOARD_ID "huntercat-nfc"
+
+#define LED_PIN PIN_PA01
+
+
+#endif
