0ebac4c43901c9b95cd5a1e6a6fde5bca361e16e
sabas1080 <s@electroniccats.com>
2020-05-03 15:26:09 -0500

    add catwan relay

---
+++ boards/catwan_relay/board_config.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Electronic Cats SAPI de CV"
+#define PRODUCT_NAME "catWAN Relay"
+#define VOLUME_LABEL "CATWAN"
+
+#define USB_VID 0x1209  
+#define USB_PID 0xBABA
+
+#define BOARD_ID "catwan-relay"
+
+#endif
