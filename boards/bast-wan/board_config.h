4a7cfdde5e83facda542e692b0bffc392b9b5fc2
sabas1080 <s@electroniccats.com>
2020-06-25 00:12:06 -0500

    new VID and PID RAK :D

--- boards/bast-wan/board_config.h
+++ boards/bast-wan/board_config.h
@@ -3,12 +3,12 @@
 
 #define CRYSTALLESS    1
 
-#define VENDOR_NAME "Electronic Cats"
+#define VENDOR_NAME "Electronic Cats and RAK"
 #define PRODUCT_NAME "BASTWAN Board"
 #define VOLUME_LABEL "BASTWAN"
 
-#define USB_VID 0x1209
-#define USB_PID 0xBAB7  
+#define USB_VID 0x0ACE
+#define USB_PID 0x4260 
 
 #define BOARD_ID "BAST-WAN"
 
