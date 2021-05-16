817451af72142ec9fdab8402b0a3a7eeb65db7c7
unixjazz <luisfelipe@softwarelivre.org>
2021-05-16 15:02:02 -0400

    added support for Big Dot board.

---
+++ boards/bigdot/board_config.h
@@ -0,0 +1,16 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "UVA SDS"
+#define PRODUCT_NAME "librelab::Big Dot"
+#define INDEX_URL "https://github.com/UVA-DSI/bigdot"
+#define VOLUME_LABEL "BIGDOT"
+
+#define USB_VID 0x1209   // Libre hardware
+#define USB_PID 0xBD00   // librelab::Big Dot
+
+#define BOARD_ID "SAML21E18-Generic"
+
+#define LED_PIN PIN_PA06
+
+#endif
