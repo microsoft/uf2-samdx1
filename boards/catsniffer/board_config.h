e18aafdb4386e8ca2286f0aaceb5fcb5f4fed27f
sabas1080 <sabasjimenez@gmail.com>
2021-07-08 13:03:32 -0500

    add catsnnifer

---
+++ boards/catsniffer/board_config.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Electronic Cats SAPI de CV"
+#define PRODUCT_NAME "CatSniffer"
+#define VOLUME_LABEL "SNIFFER"
+#define INDEX_URL "http://electroniccats.com"
+#define BOARD_ID "catsniffer"
+
+#define USB_VID 0x1209
+#define USB_PID 0xBAB9
+
+#define LED_PIN PIN_PA27
+
+#endif
