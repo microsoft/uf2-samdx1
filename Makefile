03c91c8243455bfb7f7e37c18ac3e4234aa59334
BFFonsecaYnv <bruno.fonseca@ynvisible.com>
2024-10-21 19:17:44 +0100

    Update configurations for driver v5
     - Update board.mk
     - Update board_config.h
     - Update Makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-BOARD=zero
+BOARD=driver_v5
 -include Makefile.user
 include boards/$(BOARD)/board.mk
 CC=arm-none-eabi-gcc
---
+++ boards/driver_v5/board_config.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Ynvisible SA"
+#define PRODUCT_NAME "Ynvisible Driver v5"
+#define VOLUME_LABEL "DRIVERv5"
+
+#define BOARD_ID "SAMD21J18A-Driver-v5"
+
+//#define USB_VID 0x2341
+//#define USB_PID 0x024D
+
+#define LED_PIN PIN_PA22        // LED_R
+#define LED_TX_PIN PIN_PB15     // LED_B
+#define LED_RX_PIN PIN_PA12     // LED_G
+
+#endif
